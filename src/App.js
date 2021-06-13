14dfcca0da1652dd4d98dd60d5de07b180f03b88
megha-a7 <82528894+megha-a7@users.noreply.github.com>
2021-06-13 19:37:23 +0530

    commit3

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component() {
             <h1>Search App</h1>
             <p>This is a simple search app.</p>
             
-            <SeacrhForm />
+            <SearchForm />
           </Container>  
         </Jumbotron>
         <Results />
